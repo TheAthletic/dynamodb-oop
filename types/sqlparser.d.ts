9174fee79df75d9b988b18be37fd2a14b7fbe4bf
Minh Nguyen <minh@theathletic.com>
2024-12-27 12:17:33 +1100

    Generate declaration map for the JS

---
+++ types/sqlparser.d.ts
@@ -0,0 +1,6 @@
+export { sqlparser as parser };
+export function parse(...args: any[]): any;
+export function main(args: any): any;
+/** @type {Parser} */
+declare var sqlparser: Parser;
+//# sourceMappingURL=sqlparser.d.ts.map
